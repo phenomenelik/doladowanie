07b0b5e0804480f9dfbc8c601cab1ecd08f205ca
Piotr Menel <pmenel@inpost.pl>
2024-06-18 22:18:26 +0200

    refactor

--- doladowanie.py
+++ doladowanie.py
@@ -1,5 +1,4 @@
 import os
-import time
 from pathlib import Path
 
 from selenium import webdriver
@@ -7,6 +6,7 @@ from dotenv import load_dotenv
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException, WebDriverException
@@ -25,6 +25,13 @@ CARD_YEAR = os.getenv("CARD_YEAR")
 CARD_CVV = os.getenv("CARD_CVV")
 TOP_UP = os.getenv("TOP_UP")
 
+card_data = {
+    "firstName": FIRST_NAME,
+    "lastName": LAST_NAME,
+    "cardNumber": CARD_NUM,
+    "expirationDate": CARD_MONTH + CARD_YEAR,
+    "code": CARD_CVV
+}
 
 script_directory = Path(__file__).resolve().parent
 driver_path = script_directory.joinpath("driver", "chromedriver")
@@ -39,7 +46,12 @@ def set_field_to_password(driver, element_id):
     driver.execute_script(f"document.getElementById('{element_id}').type = 'password'")
 
 
-def wait_for_element(driver, by, element_identifier, timeout=10):
+def set_fields_to_password(driver, list_of_element_ids):
+    for element_id in list_of_element_ids:
+        set_field_to_password(driver, element_id)
+
+
+def wait_for_element(driver, by, element_identifier, timeout=10) -> WebElement:
     try:
         element_present = EC.presence_of_element_located((by, element_identifier))
         WebDriverWait(driver, timeout).until(element_present)
@@ -55,25 +67,21 @@ def enter_phone_and_email(driver):
     accept_cookies_button = wait_for_element(driver, By.XPATH, "//button[text()[normalize-space()='Akceptuj wszystkie']]")
     phone_number_input = wait_for_element(driver, By.ID, "form-phone-0")
     email_input = wait_for_element(driver, By.ID, "form-email")
-    next_step_button = wait_for_element(driver, By.ID, "submit-step-1")
 
     if accept_cookies_button and phone_number_input and email_input:
-        set_field_to_password(driver, "form-phone-0")
-        set_field_to_password(driver, "form-email")
 
         accept_cookies_button.click()
         phone_number_input.send_keys(PHONE_NUMBER)
         email_input.send_keys(EMAIL)
 
-    time.sleep(4) # todo dodac legit waitera
+    next_step_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'submit-step-1')))
     next_step_button.click()
 
 
 def choose_top_up_amount(driver):
-    next_step_button = wait_for_element(driver, By.ID, "submit-step-2")
     top_up_button = wait_for_element(driver, By.XPATH, f"//li[@class='topup-amount topup-{TOP_UP}']")
     top_up_button.click()
-    time.sleep(1.5)
+    next_step_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'submit-step-2')))
     next_step_button.click()
 
 
@@ -89,6 +97,7 @@ def check_required_consents(driver):
 
 
 def payment_form(driver):
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'payway-radio-CARD')))  # coś nie hula wait_for_element
     pay_by_card_radiobutton = wait_for_element(driver, By.ID, "payway-radio-CARD")
     pay_by_card_radiobutton.click()
     driver.switch_to.frame(driver.find_element(By.ID, 'iframeCards'))
@@ -98,12 +107,7 @@ def payment_form(driver):
     expiration_date_input = wait_for_element(driver, By.ID, "expirationDate")
     cvv_input = wait_for_element(driver, By.ID, "code")
 
-    # todo ładniej ograć
-    set_field_to_password(driver, "cardNumber")
-    set_field_to_password(driver, "firstName")
-    set_field_to_password(driver, "lastName")
-    set_field_to_password(driver, "expirationDate")
-    set_field_to_password(driver, "code")
+    set_fields_to_password(driver, card_data.keys())
     card_number_input.send_keys(CARD_NUM)
     first_name_input.send_keys(FIRST_NAME)
     last_name_input.send_keys(LAST_NAME)
@@ -115,6 +119,10 @@ def payment_form(driver):
 
 
 def payment_confirmation(driver):
+    wait_for_element(driver, By.ID, "root", 10)
+
+
+def send_message():
     pass
 
 
@@ -127,6 +135,7 @@ def main():
         choose_top_up_amount(driver)
         check_required_consents(driver)
         payment_form(driver)
+        payment_confirmation(driver)
     except WebDriverException as e:
         print(f"General webdriver error {e}")
     finally:
@@ -136,4 +145,3 @@ def main():
 
 if __name__ == "__main__":
     main()
- 
